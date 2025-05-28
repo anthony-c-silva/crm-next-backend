6e2197306a87bcbc4900601ad6f018132a6803eb
anthony-c-silva <anthonycarlosp7@gmail.com>
2025-05-28 16:09:31 -0300

    feat: remover URL do segundo servidor no Swagger

--- utils/swagger.js
+++ utils/swagger.js
@@ -11,8 +11,7 @@ const options = {
                 'Documentação dos endpoints de Amostras e Pontos de Coleta',
         },
         servers: [
-            { url: 'https://crm-next-backend-1nib.vercel.app/api' , description: 'Servidor Web_1' },
-            { url: 'https://crm-next-backend-lwsj.vercel.app/api', description: 'Servidor web_2' }
+            { url: 'https://crm-next-backend-1nib.vercel.app/api' , description: 'Servidor Web_1' }
         ],
         tags: [
             { name: 'Amostras', description: 'Operações com amostras' },
