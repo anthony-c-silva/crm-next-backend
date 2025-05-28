129f5fc02a2da222694fc9c515fd41b53be0aef2
anthony-c-silva <anthonycarlosp7@gmail.com>
2025-05-28 15:54:28 -0300

    feat: atualizar URL do Swagger UI e melhorar a estrutura do código

--- pages/docs.js
+++ pages/docs.js
@@ -1,34 +1,45 @@
 // pages/docs.js
-import { useEffect } from 'react';
-import Head from 'next/head';
+import Head from 'next/head'
+import Script from 'next/script'
+import { useEffect } from 'react'
 
 export default function Docs() {
-    useEffect(() => {
-        // Carrega o SwaggerUIBundle do CDN
-        // eslint-disable-next-line no-undef
-        SwaggerUIBundle({
-            url: '/api/swagger',
-            dom_id: '#swagger-ui',
-            presets: [SwaggerUIBundle.presets.apis],
-            layout: 'BaseLayout',
-        });
-    }, []);
+  useEffect(() => {
+    // Depois que os scripts foram carregados, inicializa o Swagger UI
+    // 'SwaggerUIBundle' já estará disponível no global scope
+    // eslint-disable-next-line no-undef
+    SwaggerUIBundle({
+      url: 'https://crm-next-backend-1nib.vercel.app/api/swagger',
+      dom_id: '#swagger-ui',
+      presets: [SwaggerUIBundle.presets.apis],
+      layout: 'BaseLayout',
+    })
+  }, [])
 
-    return (
-        <>
-            <Head>
-                <meta charSet="UTF-8" />
-                <title>API Docs</title>
-                {/* CSS do Swagger UI */}
-                <link
-                    rel="stylesheet"
-                    href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css"
-                />
-                {/* JS do Swagger UI */}
-                <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js" />
-                <script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-standalone-preset.js" />
-            </Head>
-            <div id="swagger-ui" style={{ height: '100vh' }} />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>API Docs</title>
+        <meta charSet="UTF-8" />
+        {/* CSS do Swagger UI */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css"
+        />
+      </Head>
+
+      {/* Carrega o bundle JS e o standalone preset ANTES de renderizar o componente */}
+      <Script
+        src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-standalone-preset.js"
+        strategy="beforeInteractive"
+      />
+
+      {/* Container onde o Swagger UI vai ser montado */}
+      <div id="swagger-ui" style={{ height: '100vh' }} />
+    </>
+  )
 }
